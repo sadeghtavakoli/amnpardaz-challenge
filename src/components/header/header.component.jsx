60f7999af617b00cd7ed8d93a3cf2b265f67ba18
Sadegh Tavakoli <sadeghtavakoli752@gmail.com>
2021-09-06 10:47:26 +0430

    created header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,40 @@
 import React from "react";
 import "./header.styles.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 const Header = () => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <Link className="option uniqe-option" to="/">
+        Cloud Vps
+        <FontAwesomeIcon icon={faChevronLeft} className="font-awsome-icon" />
+      </Link>
+      <Link className="option" to="/">
+        CDN
+      </Link>
+      <Link className="option" to="/">
+        FIREWALL
+      </Link>
+      <Link className="option" to="/">
+        SERVER LOGS
+      </Link>
+      <Link className="option" to="/">
+        APPLICATION USERS
+      </Link>
+      <Link className="option" to="/">
+        CRONJOBS
+      </Link>
+      <Link className="option" to="/">
+        SERVICES
+      </Link>
+      <Link className="option" to="/">
+        SETTINGS
+      </Link>
+      <Link className="option" to="/">
+        ALERTS
+      </Link>
+    </header>
+  );
 };
 export default Header;
