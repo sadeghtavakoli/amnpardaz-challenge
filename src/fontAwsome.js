7c5232e014f2735c40344cce1b3ceec20ca83cf7
Sadegh Tavakoli <sadeghtavakoli752@gmail.com>
2021-09-07 10:57:47 +0430

    added fontAwsome to project

---
+++ src/fontAwsome.js
@@ -0,0 +1,26 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faServer,
+  faBars,
+  faWarehouse,
+  faShieldVirus,
+  faTextWidth,
+  faPooStorm,
+  faMoneyCheck,
+  faDoorOpen,
+  faWrench,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faServer,
+  faBars,
+  faWarehouse,
+  faPooStorm,
+  faShieldVirus,
+  faTextWidth,
+  faMoneyCheck,
+  faWrench,
+  faUser,
+  faDoorOpen
+);
