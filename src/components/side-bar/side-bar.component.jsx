2b9c73b4964ef690ea2a771cedf71e4bbe84a990
Sadegh Tavakoli <sadeghtavakoli752@gmail.com>
2021-09-07 12:02:21 +0430

    refactored seprator code in side-bar component

--- src/components/side-bar/side-bar.component.jsx
+++ src/components/side-bar/side-bar.component.jsx
@@ -27,34 +27,36 @@ const sideBarItems = [
   { id: 3, text: "فضای ذخیره سازی", className: "option", icon: "warehouse" },
   { id: 4, text: "وی پی ان", className: "option", icon: "shield-virus" },
   { id: 5, text: "پهنای باند", className: "option", icon: "text-width" },
-  { id: 6, className: "gap" },
+  { id: 6, className: "seprator" },
   { id: 7, text: "مالی", className: "option", icon: "money-check" },
   { id: 8, text: "تنظیمات", className: "option", icon: "wrench" },
   { id: 9, text: "اکانت کاربری", className: "option", icon: "user" },
   { id: 10, text: "خروج", className: "option", icon: "door-open" },
 ];
 const SideBar = () => {
-  const [selectedId, setSelectedId] = useState(0);
   const history = useHistory();
   const { pathname } = useLocation();
   return (
     <nav className="side-bar">
-      {sideBarItems.map(({ id, text, className, icon, routeName }) => (
-        <div
-          key={id}
-          className={`${className}  ${
-            pathname === routeName ? "selected" : ""
-          }`}
-          onClick={() => {
-            if (routeName) {
-              history.push(routeName);
-            }
-          }}
-        >
-          <p>{text}</p>
-          <FontAwesomeIcon icon={["fas", icon]} className="fontawsome-icon" />
-        </div>
-      ))}
+      {sideBarItems.map(({ id, text, className, icon, routeName }) => {
+        if (className === "seprator") return <hr className="seprator" />;
+        return (
+          <div
+            key={id}
+            className={`${className}  ${
+              pathname === routeName ? "selected" : ""
+            }`}
+            onClick={() => {
+              if (routeName) {
+                history.push(routeName);
+              }
+            }}
+          >
+            <p>{text}</p>
+            <FontAwesomeIcon icon={["fas", icon]} className="fontawsome-icon" />
+          </div>
+        );
+      })}
     </nav>
   );
 };
